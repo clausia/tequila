1eb09bb06279f7fef646525a3b3c82c2bc600875
Claudia ZM <clausia@ciencias.unam.mx>
2021-01-03 03:47:06 -0600

    Tests for compile_ry and compile_y

--- tests/test_recompilation_routines.py
+++ tests/test_recompilation_routines.py
@@ -1,6 +1,6 @@
 import tequila.simulators.simulator_api
 from tequila.circuit import gates
-from tequila.circuit.compiler import compile_controlled_rotation, change_basis, compile_phase, compile_swap
+from tequila.circuit.compiler import compile_controlled_rotation, change_basis, compile_phase, compile_swap, compile_ry, compile_y
 from numpy.random import uniform, randint
 from numpy import pi, isclose
 from tequila.hamiltonian import paulis
@@ -90,4 +90,28 @@ def test_compile_swap():
     equivalent_swap = gates.X(target=0, control=3) + gates.X(target=3, control=0) + gates.X(target=0, control=3)
 
     assert (equivalent_circuit == equivalent_swap)
-    
+
+
+@pytest.mark.parametrize("ang, ctrl", [(3.14, None), (6.28, 1)])
+def test_compile_ry(ang, ctrl):
+
+    circuit = gates.Ry(target=0, control=ctrl, angle=ang)
+    equivalent_circuit = compile_ry(circuit)
+
+    equivalent_ry = gates.Rz(target=0, control=None, angle=-numpy.pi / 2) \
+                    + gates.Rx(target=0, control=ctrl, angle=ang) \
+                    + gates.Rz(target=0, control=None, angle=numpy.pi / 2)
+
+    assert (equivalent_circuit == equivalent_ry)
+
+
+@pytest.mark.parametrize("ctrl", [None, 1])
+def test_compile_y(ctrl):
+    circuit = gates.Y(target=0, control=ctrl)
+    equivalent_circuit_y = compile_y(circuit)
+
+    equivalent_y = gates.Rz(target=0, control=None, angle=-numpy.pi / 2) \
+                   + gates.Rx(target=0, control=ctrl, angle=numpy.pi) \
+                   + gates.Rz(target=0, control=None, angle=numpy.pi / 2)
+
+    assert (equivalent_circuit_y == equivalent_y)
